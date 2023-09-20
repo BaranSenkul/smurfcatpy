c9369b1045763488c0076561a8a9a766375855ed
BaranSenkul <75718738+BaranSenkul@users.noreply.github.com>
2023-09-20 23:53:19 +0300

    Major update to the script

--- smurf.py
+++ smurf.py
@@ -1,21 +1,29 @@
-#made by baransenkul
+#-----------
+# smurfcatpy
+#----------
+# made by baransenkul
+#---READ---
+# If you are going to post this online, modify it or anything else. Give credit to @BaranSenkul
+# https://www.tiktok.com/@baransenkul
+# this script has been lisenced under the MIT Lisence.
+# I kinda suck at documenting my own code, So have fun tinkering!
+#----------
 
 import tkinter as tk
 from tkinter import ttk
 import random
 import threading
 import playsound
-import time
 
 def play_sound():
     playsound.playsound("spectre.mp3")
 
 def create_window_with_text(text, font, x, y):
     window = tk.Toplevel(root)
-    window.title(text)
+    window.title("Public ver. Made by BaranSenkul")
     
     label = tk.Label(window, text=text, font=font)
-    label.update_idletasks()  # Ensure label is updated to compute its size
+    label.update_idletasks()  # Ensure the text is updated to compute its size
   
     window.geometry(f"400x100+{x}+{y}")
     
@@ -27,24 +35,17 @@ sound_thread = threading.Thread(target=play_sound)
 sound_thread.start()
 
 root = tk.Tk()
-root.title("WE LIVE")
+root.title("Public ver. Made by BaranSenkul")
 
-# After 4.73 seconds, create the "WE LIVE" window
-root.after(4730, create_window_with_text, "WE LIVE", ("bold", 32), random.randint(50, 150), random.randint(50, 150))
-
-# After 1.2 seconds, create the "WE LOVE" window on the middle right side
-root.after(5930, create_window_with_text, "WE LOVE", ("bold", 36), root.winfo_screenwidth() - 450, root.winfo_screenheight() // 2 - 100)
-
-# After 1.6 seconds, create the "WE LIE" window with a larger font at the bottom
-root.after(6700, create_window_with_text, "WE LIE", ("italic bold", 48), root.winfo_screenwidth() // 2 - 100, root.winfo_screenheight() - 150)
+root.after(5130, create_window_with_text, "WE LIVE", ("Verdana", 32), random.randint(50, 150), random.randint(50, 150))
+root.after(5930, create_window_with_text, "WE LOVE", ("Verdana", 36), root.winfo_screenwidth() - 450, root.winfo_screenheight() // 2 - 100)
+root.after(6950, create_window_with_text, "WE LIE", ("Verdana", 48), root.winfo_screenwidth() // 2 - 100, root.winfo_screenheight() - 165)
 
 image = tk.PhotoImage(file="smurfcat.png")
 
 def display_image():
     window_image = tk.Toplevel(root)
     window_image.title("Image")
-    
-    # Adjust the window size to fit the image
     window_width = image.width()
     window_height = image.height()
     window_x = root.winfo_screenwidth() // 2 - window_width // 2
@@ -55,7 +56,9 @@ def display_image():
     label_image = tk.Label(window_image, image=image)
     label_image.pack()
 
-root.after(7630, display_image)
+root.after(7730, display_image)
 
-root.withdraw()  # Hide the main window
+root.withdraw() 
 root.mainloop()
+
+# https://www.tiktok.com/@baransenkul
